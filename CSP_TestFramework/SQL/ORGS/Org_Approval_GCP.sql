4e6f788d8a2b7e3d92df4727970f40dca0c398d0
dangetirk <123734024+dangetirk@users.noreply.github.com>
2023-08-16 16:07:42 +0100

    added orgs approval

--- CSP_TestFramework/SQL/ORGS/Org_Approval_GCP.sql
+++ CSP_TestFramework/SQL/ORGS/Org_Approval_GCP.sql
@@ -1,1 +1,20 @@
-
+    SELECT
+    Id
+    ,OwnerId
+    ,Name
+    ,CurrencyIsoCode
+    ,CONCAT(REPLACE(CAST(CreatedDate AS STRING ),' ','T'),'.000+0000') AS CreatedDate
+    ,CreatedById
+    ,CONCAT(REPLACE(CAST(LastModifiedDate AS STRING ),' ','T'),'.000+0000') AS LastModifiedDate
+    ,LastModifiedById
+    ,CCS_Approval_for__c
+    ,CCS_Comments__c
+    ,CCS_Connection__c
+    ,CCS_Lending_Group__c
+    ,CCS_ORG_Lead__c
+    ,CCS_ORG_Members__c
+    ,CCS_ORG_Status__c
+    ,CCS_Request_Reviewer__c
+    ,CCS_Review_Date__c
+    ,CCS_Reviewer_Comments__c
+    FROM dmn01-rsksoi-bld-01-2017.dmn01_rsksoi_euwe2_rsk_csp_ds_curation.rskcsp_ds_org_approval_curated
--- CSP_TestFramework/SQL/ORGS/Org_Approval_NCINO.sql
+++ CSP_TestFramework/SQL/ORGS/Org_Approval_NCINO.sql
@@ -1,1 +1,21 @@
-
+    SELECT
+    Id
+    ,OwnerId
+    ,Name
+    ,CurrencyIsoCode
+    ,CreatedDate
+    ,CreatedById
+    ,LastModifiedDate
+    ,LastModifiedById
+    ,CCS_Approval_for__c
+    ,CCS_Comments__c
+    ,CCS_Connection__c
+    ,CCS_Lending_Group__c
+    ,CCS_ORG_Lead__c
+    ,CCS_ORG_Members__c
+    ,CCS_ORG_Status__c
+    ,CCS_Request_Reviewer__c
+    ,CCS_Review_Date__c
+    ,CCS_Reviewer_Comments__c
+    FROM CCS_ORG_Approval__c
+    where CreatedDate >=2023-08-16T00:00:00.000Z
